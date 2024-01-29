4d8b57b50556eb51a4b8aa1f45e1b6e803262484
LAPTOP-L04T4SLQ\suhye <suhyeon5280@gmail.com>
2024-01-29 13:42:12 +0900

    code1

---
+++ 1.py
@@ -0,0 +1,26 @@
+import turtle
+
+import random as r
+
+co=['red','pink','skyblue','blue','orange','green']
+
+tu=turtle.Turtle()
+
+sn=turtle.Screen()
+
+sn.setup(500,500)
+
+for n in range(40):
+
+    tu=turtle.Turtle()
+
+    tu.shape('turtle')
+
+    tu.penup()
+
+    x,y=r.randint(-200,200),r.randint(-200,200)
+
+    tu.color(co[r.randint(0,5)],co[r.randint(0,5)])   #앞에가 테두리 뒤에가 색
+
+    tu.goto(x,y)
+
